16f433b1f21862f853e1aff8cae3ab03c51e9857
Balazs Braun <lucksmith.wd@gmail.com>
2023-10-20 19:44:51 +0800

    Updated template files

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import htmlPurge from "vite-plugin-purgecss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), htmlPurge()],
+});
